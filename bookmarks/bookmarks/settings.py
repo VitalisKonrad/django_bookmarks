b0066c8840f80e5a5a7602c7230c50dcdeb09c4b
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-03-19 17:50:36 +0300

    add new app images

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'images.apps.ImageConfig',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
+from django.urls import reverse
 
 class Image(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE) #При удалении пользователя все его картинки удалятся
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, blank=True)
-    url = models.URLField()
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
-    description = models.DateField(auto_now_add=True, db_index=True)
+    slug = models.SlugField(max_length=200, blank=True) #Для семантических url`ов
+    url = models.URLField() #Ссылка на оригинал
+    image = models.ImageField(upload_to='images/%Y/%m/%d/') #Сам файл
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True, db_index=True)#По этому полю создается индекс в БД
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     def __str__(self):
         return self.title
+
+    #Переопределяем метод save. Сохраняем по слагу, если его нет, то по заголовку
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Image, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+            return reverse('images:detail', args=[self.id, self.slug])
